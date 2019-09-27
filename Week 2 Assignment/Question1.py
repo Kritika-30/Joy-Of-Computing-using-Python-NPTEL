2bb8b9394fa91ba53c6a3bd695abb795445f60cc
Kritika-30 <51390413+Kritika-30@users.noreply.github.com>
2019-09-27 18:33:24 +0530

    Addition

---
+++ Week 2 Assignment/Question1.py
@@ -0,0 +1,20 @@
+'''In this assignment, you will have to take two numbers (integers) as input and print the addition.
+
+Input Format:
+
+The first line of the input contains two numbers separated by a space.
+
+Output Format:
+
+Print the addition in single line'''
+
+
+#CODE
+
+# input
+x,y=input().split()
+x=int(x)
+y=int(y)
+  
+# printing the sum in integer 
+print(x+y)
